2f870a6080922e725c2fd4ac31a5f7673e417453
Kaloyan Stefanov <kokmarok@gmail.com>
2025-05-19 21:28:02 +0300

    update middeware matcher

--- middleware.ts
+++ middleware.ts
@@ -23,6 +23,6 @@ export default clerkMiddleware(async (auth, req) => {
 
 export const config = {
     matcher: [
-       "/((?!_next/static|_next/image|favicon.ico).*)"
+        "/((?!_next/static|_next/image|favicon.ico|.*\\.svg|.*\\.png|.*\\.jpg|.*\\.jpeg).*)"
     ],
 }
