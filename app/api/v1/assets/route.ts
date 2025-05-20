f334e720369f5def6a93413cf438d40460fafc84
Kaloyan Stefanov <kokmarok@gmail.com>
2025-05-20 11:14:41 +0300

    fix sections and videos order

--- app/api/v1/assets/route.ts
+++ app/api/v1/assets/route.ts
@@ -11,7 +11,7 @@ const s3 = new S3Client({
         accessKeyId: process.env.R2_ACCESS_KEY_ID!,
         secretAccessKey: process.env.R2_SECRET_ACCESS_KEY!
     }
-})
+});
 
 export async function GET(req: Request, res: Response) {
     try {
