bfa9be82d899eced91605a309645a3bb5534802b
Kaloyan Stefanov <kokmarok@gmail.com>
2025-05-24 22:49:38 +0300

    use standard <img /> for shop's module images

--- app/_components/dashboard/shop/module-box/DashboardShopModuleBox.tsx
+++ app/_components/dashboard/shop/module-box/DashboardShopModuleBox.tsx
@@ -16,7 +16,7 @@ export default function DashboardShopModuleBox({module, boughtModules}: Dashboar
     return <DashboardShopModuleBoxClientWrapper moduleId={module.id} stripeProductId={module.stripe_product_id} boughtModules={boughtModules}>
         <Card className="cursor-pointer flex flex-col justify-between hover:bg-secondary/70 dark:hover:bg-border/50">
             <div className="relative w-full aspect-video rounded-t-lg overflow-hidden">
-                {module.image_url && <Image alt={module.title} src={module.image_url} fill/>}
+                {module.image_url && <img alt={module.title} src={module.image_url} className="absolute inset-0 object-cover" />}
             </div>
             <CardHeader className="pt-3 pb-0">
                 <CardTitle>{module.title}</CardTitle>
