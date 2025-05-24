a058f33575fedd17d45b0db45b988d5fce003195
Kaloyan Stefanov <kokmarok@gmail.com>
2025-05-24 21:08:43 +0300

    refactor navbar

--- app/_components/home/nav/HomepageMobileNavbar.tsx
+++ app/_components/home/nav/HomepageMobileNavbar.tsx
@@ -1,40 +1,50 @@
 "use client"
 
-import {
-    Sheet,
-    SheetContent,
-    SheetDescription, SheetFooter,
-    SheetHeader,
-    SheetTitle,
-    SheetTrigger
-} from "@/app/_components/ui/shadcn/sheet";
-import {IoMenuOutline} from "react-icons/io5";
+import {useEffect, useState} from "react";
+import {IoClose, IoMenuOutline} from "react-icons/io5";
+import Image from "next/image";
+import HomepageNavLinks from "@/app/_components/home/nav/HomepageNavLinks";
 import SecondaryButton from "@/app/_components/ui/buttons/SecondaryButton";
 import {Routes} from "@/app/_utils/nav/routes";
 import PrimaryButton from "@/app/_components/ui/buttons/PrimaryButton";
-import HomepageNavLinks from "@/app/_components/home/nav/HomepageNavLinks";
-import {useState} from "react";
+import {cn} from "@/app/_utils/cn";
 
 export default function HomepageMobileNavbar() {
-
     const [opened, setOpened] = useState(false);
 
-    return <Sheet open={opened}>
-        <SheetTrigger onClick={() => setOpened(true)}><IoMenuOutline className="text-3xl"/></SheetTrigger>
-        <SheetContent className="flex flex-col justify-between px-0">
-            <div className="grid">
-                <SheetHeader className="px-5">
-                    {/*<Image alt="I&M NextGen Academy" src="/logo.svg" width={150} height={150} />*/}
-                    <SheetTitle className="text-left">Навигация</SheetTitle>
-                    <SheetDescription className="text-left">Избери някоя от секциите или влез в профила си</SheetDescription>
-                </SheetHeader>
-                <hr className="w-full h-[1px] bg-border my-3"/>
-                <HomepageNavLinks onSelected={() => setOpened(false)} />
+    useEffect(() => {
+        if (opened) document.documentElement.style.overflow = "hidden";
+        else document.documentElement.style.overflow = "auto";
+
+        return () => {
+            document.documentElement.style.overflowY = "auto";
+        }
+    }, [opened]);
+
+    return <div>
+        <button onClick={() => setOpened(true)}><IoMenuOutline className="text-4xl" /></button>
+
+        {opened && <div className="fixed inset-0 bg-transparent z-20" onClick={() => setOpened(false)} />}
+
+        <div
+            style={{transform: `translateX(${opened ? 0 : 100}%)`}}
+            className={cn(
+                "fixed top-0 right-0 w-[75vw] max-w-[20rem] h-full grid grid-rows-[auto_auto_1fr] bg-background shadow-2xl rounded-lg py-3 z-20",
+                "transition-all duration-300 ease-in-out"
+            )}
+        >
+            <div className="grid grid-cols-[1fr_auto] px-3">
+                <Image alt="I&M NextGen Academy" src="/logo.svg" width={170} height={100} />
+                <button onClick={() => setOpened(false)}><IoClose className="text-2xl text-primary/70" /></button>
             </div>
-            <SheetFooter className="flex flex-wrap gap-y-3 px-5">
-                <SecondaryButton href={Routes.auth.signIn()} linkClassName="flex-1" className="w-full">Влизане</SecondaryButton>
-                <PrimaryButton href={Routes.auth.signUp()} linkClassName="flex-1" className="w-full">Регистрация</PrimaryButton>
-            </SheetFooter>
-        </SheetContent>
-    </Sheet>
+            <hr className="w-full h-[1px] bg-border my-3"/>
+            <div className="px-3 flex flex-col justify-between">
+                <HomepageNavLinks onSelect={() => setOpened(false)} />
+                <div className="grid gap-2">
+                    <SecondaryButton href={Routes.auth.signIn()} className="w-full">Влизане</SecondaryButton>
+                    <PrimaryButton href={Routes.auth.signUp()} className="w-full">Регистрация</PrimaryButton>
+                </div>
+            </div>
+        </div>
+    </div>
 }
--- app/_components/home/nav/HomepageNavLinks.tsx
+++ app/_components/home/nav/HomepageNavLinks.tsx
@@ -1,52 +1,16 @@
 "use client"
 
-import {
-    NavigationMenu,
-    NavigationMenuItem,
-    NavigationMenuList,
-    navigationMenuTriggerStyle
-} from "@/app/_components/ui/shadcn/navigation-menu";
 import {HOME_NAV_ITEMS} from "@/app/_utils/home/nav/homepage-navbar-items";
-import {cn} from "@/app/_utils/cn";
-import {useRouter} from "next/navigation";
-import {useViewLoaded} from "@/app/_hooks/useViewLoaded";
-import {useWindowWidth} from "@react-hook/window-size";
+import Link from "next/link";
 
 type HomepageNavLinksProps = {
-    onSelected?: () => void
+    onSelect?: () => void
 }
 
-export default function HomepageNavLinks({onSelected}: HomepageNavLinksProps) {
-    const {viewLoaded} = useViewLoaded();
-    const width = useWindowWidth();
-    const router = useRouter();
-
-    function closeMenusAndScroll(currHash?: string) {
-        if (!currHash) return;
-        if (onSelected) onSelected();
-        // Add delay because the sheet component sets the scroll position to 0 after closing
-        setTimeout(() => {
-            router.push(currHash);
-        }, width > 1020 ? 0 : 350);
-    }
-
-    if (!viewLoaded) return;
-
-    return <NavigationMenu className="homepage-mobile-navbar-inner-container test w-full max-w-full">
-        <NavigationMenuList className="flex-col lg:flex-row w-full">
-            {HOME_NAV_ITEMS.map((item, index) => {
-                return <NavigationMenuItem key={index} className="w-full">
-                    <button
-                        className={cn(
-                            navigationMenuTriggerStyle(),
-                            "bg-transparent font-normal text-[1rem] w-full"
-                        )}
-                        onClick={() => closeMenusAndScroll(item.href)}
-                    >
-                        {item.label}
-                    </button>
-                </NavigationMenuItem>
-            })}
-        </NavigationMenuList>
-    </NavigationMenu>
+export default function HomepageNavLinks({onSelect}: HomepageNavLinksProps) {
+    return <nav className="flex flex-col lg:flex-row lg:items-center gap-3 lg:gap-5">
+        {HOME_NAV_ITEMS.map((item, index) => {
+            return <Link onClick={onSelect} className="text-[1.1rem] lg:text-[1rem] hover:text-cta transition-all duration-200" href={item.href} key={index}>{item.label}</Link>
+        })}
+    </nav>
 }
