bc7e5f863953bc8b8c6f15b57f41a65eb9efa270
Kaloyan Stefanov <kokmarok@gmail.com>
2025-05-24 22:16:47 +0300

    (dev) return clerk webhook secret in response

--- app/api/v1/webhooks/protect.ts
+++ app/api/v1/webhooks/protect.ts
@@ -4,16 +4,16 @@ import {NextResponse} from "next/server";
 export function clerkWebhookProtect(secret: string, payload: string, headers: Headers): NextResponse | null {
     try {
         const wh = new Webhook(secret);
-        console.log("Authenticating clerk webhook...!");
+        
         wh.verify(payload, {
             "svix-id": headers.get("svix-id") ?? "",
             "svix-timestamp": headers.get("svix-timestamp") ?? "",
             "svix-signature": headers.get("svix-signature") ?? ""
         });
-        console.log("Clerk webhook authenticated!");
+
         return null;
     } catch(e) {
         console.log(`Clerk webhook authorization error: ${e}`);
-        return NextResponse.json({status: "fail", error: "Unauthorized"}, {status: 401});
+        return NextResponse.json({status: "fail", error: "Unauthorized", secret: process.env.CLERK_USER_DELETED_WEBHOOK_SECRET}, {status: 401});
     }
 }
