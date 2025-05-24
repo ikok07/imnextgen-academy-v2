659a769c6facd39596f46e858199642aaec75358
Kaloyan Stefanov <kokmarok@gmail.com>
2025-05-24 22:12:18 +0300

    add even more logs

--- app/api/v1/webhooks/protect.ts
+++ app/api/v1/webhooks/protect.ts
@@ -4,16 +4,16 @@ import {NextResponse} from "next/server";
 export function clerkWebhookProtect(secret: string, payload: string, headers: Headers): NextResponse | null {
     try {
         const wh = new Webhook(secret);
-
+        console.log("Authenticating clerk webhook...!");
         wh.verify(payload, {
             "svix-id": headers.get("svix-id") ?? "",
             "svix-timestamp": headers.get("svix-timestamp") ?? "",
             "svix-signature": headers.get("svix-signature") ?? ""
         });
-        console.log("Webhook authenticated!");
+        console.log("Clerk webhook authenticated!");
         return null;
     } catch(e) {
-        console.log(`Webhooks Authorization Error: ${e}`);
+        console.log(`Clerk webhook authorization error: ${e}`);
         return NextResponse.json({status: "fail", error: "Unauthorized"}, {status: 401});
     }
 }
