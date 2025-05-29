44329b52537f1369578788326ffe41a65d96e3b0
Kaloyan Stefanov <kokmarok@gmail.com>
2025-05-29 21:24:17 +0300

    floor event's start date

--- next.config.mjs
+++ next.config.mjs
@@ -10,11 +10,7 @@ const nextConfig = {
             {
                 protocol: "https",
                 hostname: "img.clerk.com"
-            },
-            {
-                protocol: "https",
-                hostname: "beta.academy.imnextgen.bg"
-            },
+            }
         ]
     }
 };
