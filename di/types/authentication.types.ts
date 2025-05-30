717f2ff71328c395e5f4d25cfb174bcee836ed91
Kaloyan Stefanov <kokmarok@gmail.com>
2025-05-30 11:01:19 +0300

    add getAllUsersForRole method

--- di/types/authentication.types.ts
+++ di/types/authentication.types.ts
@@ -1,19 +1,25 @@
 import {IAuthenticationService} from "@/src/application/services/auth/authentication.service.interface";
+import { IGetAllUsersForRoleUseCase } from "@/src/application/use-cases/auth/get-all-users-for-role.use-case";
 import {IGetUserUseCase} from "@/src/application/use-cases/auth/get-user.use-case";
+import { IGetAllUsersForRoleController } from "@/src/interface-adapters/controllers/auth/get-all-users-for-role.controller";
 import {IGetUserController} from "@/src/interface-adapters/controllers/auth/get-user.controller";
 
 export const AUTH_SYMBOLS = {
     IAuthenticationService: Symbol.for("IAuthenticationService"),
 
     IGetUserUseCase: Symbol.for("IGetUserUseCase"),
+    IGetAllUsersForRoleUseCase: Symbol.for("IGetAllUsersForRoleUseCase"),
 
-    IGetUserController: Symbol.for("IGetUserController")
+    IGetUserController: Symbol.for("IGetUserController"),
+    IGetAllUsersForRoleController: Symbol.for("IGetAllUsersForRoleController")
 }
 
 export interface AUTH_RETURN_TYPES {
     IAuthenticationService: IAuthenticationService,
 
     IGetUserUseCase: IGetUserUseCase,
+    IGetAllUsersForRoleUseCase: IGetAllUsersForRoleUseCase,
 
     IGetUserController: IGetUserController,
+    IGetAllUsersForRoleController: IGetAllUsersForRoleController
 }
--- src/infrastructure/services/auth/clerk.service.ts
+++ src/infrastructure/services/auth/clerk.service.ts
@@ -1,9 +1,16 @@
-import {IAuthenticationService} from "@/src/application/services/auth/authentication.service.interface";
-import {User, AuthObject} from "@clerk/backend";
+import {
+    GetAllUsersForRoleOptions,
+    GetAllUsersForRoleResponse,
+    IAuthenticationService
+} from "@/src/application/services/auth/authentication.service.interface";
+import {User, AuthObject, createClerkClient} from "@clerk/backend";
 import {auth, currentUser} from "@clerk/nextjs/server";
 import {AuthenticationError} from "@/src/entities/errors/auth/authentication";
+import {UserRole} from "@/src/entities/models/auth/user-roles";
 
 export class ClerkService implements IAuthenticationService {
+    client = createClerkClient({secretKey: process.env.CLERK_SECRET_KEY});
+
     async getUser(): Promise<{user: User | null, auth: AuthObject}> {
         try {
             const authObject = await auth();
@@ -14,6 +21,19 @@ export class ClerkService implements IAuthenticationService {
             };
         } catch(e) {
             throw new AuthenticationError(`Failed to get user! ${e}`);
+        }
+    }
+    async getAllUsersForRole({role}: GetAllUsersForRoleOptions): Promise<GetAllUsersForRoleResponse> {
+        try {
+            const {data} = await this.client.users.getUserList();
+            const filteredUsers = data.filter(user => (user.publicMetadata["roles"] as string[] | undefined)?.includes(role));
+
+            return {
+                data: filteredUsers,
+                count: filteredUsers.length
+            }
+        } catch(e) {
+            throw new AuthenticationError(`Failed to get users for role! ${e}`);
         }
     }
 }
