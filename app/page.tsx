e82275452d0bf3b7a1c94cff65362a1c200fe54c
Kaloyan Stefanov <kokmarok@gmail.com>
2025-05-31 09:28:03 +0300

    fix stripe checkout session caching

--- app/page.tsx
+++ app/page.tsx
@@ -2,6 +2,7 @@ import LanguageSelector from "@/app/_components/ui/language/LanguageSelector";
 import {getTranslations} from "next-intl/server";
 import AuthComponent from "@/app/_components/AuthComponent";
 import ThemeSelector from "@/app/_components/ui/theme/ThemeSelector";
+import TableComponent from "@/app/_components/TableComponent";
 
 export default async function Home() {
   const t = await getTranslations("Homepage");
@@ -15,5 +16,6 @@ export default async function Home() {
       <h1>{t("title")}</h1>
     </div>
     <AuthComponent />
+    <div className="w-[40rem] h-[10rem]"><TableComponent /></div>
   </div>
 }
