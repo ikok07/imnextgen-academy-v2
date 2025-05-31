6b4ec3488bfb84ba718dc96c22d6f474b769cea4
Kaloyan Stefanov <kokmarok@gmail.com>
2025-05-31 09:34:41 +0300

    fix bug with shadcn table

--- app/_components/ui/shadcn/table.tsx
+++ app/_components/ui/shadcn/table.tsx
@@ -3,16 +3,19 @@ import * as React from "react"
 import { cn } from "@/app/_utils/cn"
 
 const Table = React.forwardRef<
-  HTMLTableElement,
-  React.HTMLAttributes<HTMLTableElement>
->(({ className, ...props }, ref) => (
-  <div className="relative w-full overflow-auto">
-    <table
-      ref={ref}
-      className={cn("w-full caption-bottom text-sm", className)}
-      {...props}
-    />
-  </div>
+    HTMLTableElement,
+    React.HTMLAttributes<HTMLTableElement> & {wrapperDivClassName?: string}
+>(({ className, wrapperDivClassName, ...props }, ref) => (
+    <div className={cn(
+        "relative w-full overflow-auto",
+        wrapperDivClassName
+    )}>
+        <table
+            ref={ref}
+            className={cn("w-full caption-bottom text-sm", className)}
+            {...props}
+        />
+    </div>
 ))
 Table.displayName = "Table"
 
