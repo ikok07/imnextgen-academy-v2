568429cfe803c1e8b4941e8cafe05179b91457ba
Kaloyan Stefanov <kokmarok@gmail.com>
2025-06-04 19:54:00 +0300

    add more logging

--- src/infrastructure/services/auth/clerk.service.ts
+++ src/infrastructure/services/auth/clerk.service.ts
@@ -25,6 +25,8 @@ export class ClerkService implements IAuthenticationService {
     async getAllUsersForRole({role}: GetAllUsersForRoleOptions): Promise<GetAllUsersForRoleResponse> {
         try {
             const {data} = await this.client.users.getUserList();
+            console.log("ADMIN DATA:")
+            console.log(data);
             const filteredUsers = data.filter(user => (user.publicMetadata["roles"] as string[] | undefined)?.includes(role));
 
             return {
