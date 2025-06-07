800fc8e379171993d4ec5f1f831f64bcf8594c28
Kaloyan Stefanov <kokmarok@gmail.com>
2025-06-07 23:57:44 +0300

    fix small bug

--- app/_components/dashboard/admin/users/modal/sales-meetings/AdminUserDetailsSalesMeetings.tsx
+++ app/_components/dashboard/admin/users/modal/sales-meetings/AdminUserDetailsSalesMeetings.tsx
@@ -13,8 +13,8 @@ type AdminUserDetailsSalesMeetingsProps = {
 // TODO: 1. Fix 'too much recursion' error when opening select menu
 // TODO: 2. Integrate google calendar webhooks
 // TODO: 3. Develop update event functionality
-// TODO: 4. Add option for variable event durations
-// TODO: 5. Show the user it's sales meetings
+// TODO: 4. Add option for variable event durations X
+// TODO: 5. Show the user it's sales meetings X
 // TODO: 6. Fix access policies
 
 export default function AdminUserDetailsSalesMeetings({fullProfile}: AdminUserDetailsSalesMeetingsProps) {
