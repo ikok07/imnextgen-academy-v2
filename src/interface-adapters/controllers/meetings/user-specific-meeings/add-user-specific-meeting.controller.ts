e0eb6c3b7cb4038aae7d79aedad01155300e2501
Kaloyan Stefanov <kokmarok@gmail.com>
2025-06-07 11:14:06 +0300

    protect specific meetings controllers

--- src/interface-adapters/controllers/meetings/user-specific-meeings/add-user-specific-meeting.controller.ts
+++ src/interface-adapters/controllers/meetings/user-specific-meeings/add-user-specific-meeting.controller.ts
@@ -2,11 +2,34 @@ import {
     IAddUserSpecificMeetingUseCase
 } from "@/src/application/use-cases/meetings/user-specific-meetings/add-user-specific-meeting.use-case";
 import {UserSpecificMeetingInsert} from "@/drizzle/schema/user_specific_meetings";
+import {AccessError} from "@/src/entities/models/auth/access";
+import {IGetUserController} from "@/src/interface-adapters/controllers/auth/get-user.controller";
+import {ICheckAccessController} from "@/src/interface-adapters/controllers/auth/check-access.controller";
 
 export type IAddUserSpecificMeetingController = ReturnType<typeof addUserSpecificMeetingController>;
 
 export const addUserSpecificMeetingController = (
-    addUserSpecificMeetingUseCase: IAddUserSpecificMeetingUseCase
+    addUserSpecificMeetingUseCase: IAddUserSpecificMeetingUseCase,
+    getUserController: IGetUserController,
+    checkAccessController: ICheckAccessController
 ) => async (data: UserSpecificMeetingInsert) => {
+
+    const {user, dbProfile} = await getUserController();
+    if (!user || !dbProfile) throw new AccessError("Could not verify access! User could not be found!");
+
+    const hasAccess = await checkAccessController({
+        principal: {
+            id: user.id,
+            roles: dbProfile.roles
+        },
+        resource: {
+            id: "specific-meeting",
+            kind: "specific-meeting",
+        },
+        action: "create"
+    });
+
+    if (!hasAccess) throw new AccessError("Unauthorized action!");
+
     return addUserSpecificMeetingUseCase(data);
 }
--- src/interface-adapters/controllers/meetings/user-specific-meeings/get-specific-meetings-by-user-id.controller.ts
+++ src/interface-adapters/controllers/meetings/user-specific-meeings/get-specific-meetings-by-user-id.controller.ts
@@ -6,15 +6,39 @@ import {
     GetSpecificMeetingsByUserIdOptions, getSpecificMeetingsByUserIdOptionsSchema
 } from "@/src/application/repositories/meetings/user-specific-meetings.repository.interface";
 import {ICheckAccessController} from "@/src/interface-adapters/controllers/auth/check-access.controller";
+import {IGetUserController} from "@/src/interface-adapters/controllers/auth/get-user.controller";
+import {AccessError} from "@/src/entities/models/auth/access";
 
 export type IGetSpecificMeetingsByUserIdController = ReturnType<typeof getSpecificMeetingsByUserIdController>;
 
 export const getSpecificMeetingsByUserIdController = (
-    getSpecificMeetingsByUserIdUseCase: IGetSpecificMeetingsByUserIdUseCase
+    getSpecificMeetingsByUserIdUseCase: IGetSpecificMeetingsByUserIdUseCase,
+    getUserController: IGetUserController,
+    checkAccessController: ICheckAccessController
 ) => async (opts: Partial<GetSpecificMeetingsByUserIdOptions>) => {
 
     const {data: parsedOpts, error} = getSpecificMeetingsByUserIdOptionsSchema.safeParse(opts);
     if (error) throw new InputParseError(`Invalid options! ${error}`);
+
+    const {user, dbProfile} = await getUserController();
+    if (!user || !dbProfile) throw new AccessError("Could not verify access! User could not be found!");
+
+    const hasAccess = await checkAccessController({
+        principal: {
+            id: user.id,
+            roles: dbProfile.roles
+        },
+        resource: {
+            id: "specific-meeting",
+            kind: "specific-meeting",
+            attr: {
+                profile_id: opts.userId
+            }
+        },
+        action: "select"
+    });
+
+    if (!hasAccess) throw new AccessError("Unauthorized action!");
 
     return getSpecificMeetingsByUserIdUseCase(parsedOpts);
 }
