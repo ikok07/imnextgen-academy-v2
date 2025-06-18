55aed70942ae596b0c9da287623b375e39f4d7ac
Kaloyan Stefanov <kokmarok@gmail.com>
2025-06-18 19:21:35 +0300

    allow primary selection options to be visible when no selection

--- app/_components/ui/tables/provider/selection-options.ts
+++ app/_components/ui/tables/provider/selection-options.ts
@@ -4,6 +4,7 @@ import {IconType} from "react-icons";
 
 type SelectionDisabledOptions = {
     enabled: false,
+    showWhenNoSelection?: undefined,
     multipleSelection?: undefined,
     selectedRows?: undefined,
     onRowSelected?: undefined,
@@ -11,11 +12,17 @@ type SelectionDisabledOptions = {
 }
 type SelectionEnabledOptions = {
     enabled: true,
+    showWhenNoSelection?: boolean,
     multipleSelection?: boolean,
     selectedRows: RowSelectionState,
     onRowSelected: Dispatch<SetStateAction<RowSelectionState>>,
     onDelete?: (rows: Row<any>[]) => void,
-    otherOptions?: {Icon: IconType, label: string, onClick: (rows: Row<any>[]) => void, className?: string}[]
+    otherOptions?: {
+        Icon: IconType,
+        label: string,
+        onClick: (rows: Row<any>[]) => void,
+        className?: string
+    }[]
 }
 
 export type SelectionOptions = SelectionDisabledOptions | SelectionEnabledOptions;
